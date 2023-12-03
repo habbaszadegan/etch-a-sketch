811490d3635456c2e132701422fa17fe22eacfa3
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-03 09:52:02 +0330

    Changed JS inline CSS to separate CSS file.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ for (let i = 1; i <= 256; i++) {
     divElement.id = `div${i}`;
     // divElement.textContent = i;
     container.appendChild(divElement);
-    divElement.setAttribute('style', 'gap: 5px;')
+    divElement.classList.add('divElement');
     // divElement.addEventListener('mouseover', () => {
     //     divElement.setAttribute('style', 'color: blue; background: white; width: 100%;');
     // })
