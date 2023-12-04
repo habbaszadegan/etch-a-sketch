a1d78b602f1590406be5825d09b922c0dee7d08f
Hossein Abbaszadegan <habbaszadegan@gmail.com>
2023-12-04 13:16:57 +0330

    No significant changes have been made

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('#container');
+
 for (let i = 1; i <= 256; i++) {
     const divElement = document.createElement('div');
     divElement.id = `div${i}`;
@@ -8,3 +10,9 @@ for (let i = 1; i <= 256; i++) {
     //     divElement.setAttribute('style', 'color: blue; background: white; width: 100%;');
     // })
 }
+
+const containerWidth = container.offsetWidth;
+const elementWidth = Math.floor(containerWidth / 16);
+
+divElement.style.width = `${elementWidth}px`;
+divElement.style.height = `${elementWidth}px`;
