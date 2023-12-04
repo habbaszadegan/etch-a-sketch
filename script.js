8a1b9f064f92d6a7804d4cdfc83f53b84b8df1fd
Hossein Abbaszadegan <haabaszadegan@gmail.com>
2023-12-04 16:56:10 +0330

    Further modified eListener for gridSizeInput
    added clauses ensuring input is a number only between 1 & 100.
    stores user input into variable gridNum.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector('#container');
 const gridSizeInput = document.querySelector('#gridSizeInput');
 const gridSizeOutput = document.querySelector('#gridSizeOutput');
 
+let gridNum = 16;
+
 // function lessThan100 (num) {
 //     if (num > 100 || num < 1) {
 //         return "Please input number between 1 and 100"
@@ -9,17 +11,22 @@ const gridSizeOutput = document.querySelector('#gridSizeOutput');
 // }
 
 gridSizeInput.addEventListener('click', () => {
-    let gridNum = prompt('Grid size? Input between 1 - 100', '');
-    if (gridNum > 100 || gridNum < 1) {
-        return "please input number between 1 and 100";
+    gridNum = prompt('Grid size? Input between 1 - 100', '');
+    if (gridNum === null || gridNum === '') {
+        return;
     }
+    gridNum = parseInt(gridNum);
+    if (isNaN(gridNum) || gridNum > 100 || gridNum < 1) {
+        alert("please input number between 1 and 100");
+        return;
+    }
+    console.log(`gridNum is: ${gridNum}`);
 })
 
 // store user input in variable. use that variable to create new grid.
 
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= gridNum * gridNum; i++) {
     const divElement = document.createElement('div');
-    divElement.id = `div${i}`;
     container.appendChild(divElement);
     divElement.classList.add('divElement');
     divElement.addEventListener('mouseover', () => {
